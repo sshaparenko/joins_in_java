c44b13c24badec89362f9f12dd1cadbc84c5191b
sshaparenko <shaparenko.archicgi@gmail.com>
2022-11-19 23:00:26 +0200

    add tests

---
+++ src/test/java/shaparenko/task/InnerJoinOperationTest.java
@@ -0,0 +1,62 @@
+package shaparenko.task;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InnerJoinOperationTest {
+
+    @Test
+    void join() {
+        List<DataRow<Integer, String>> leftRowListTest = new ArrayList<>();
+        leftRowListTest.add(new DataRow<>(0, "Ukraine"));
+        leftRowListTest.add(new DataRow<>(1, "Germany"));
+        leftRowListTest.add(new DataRow<>(2, "France"));
+
+        List<DataRow<Integer, String>> rightRowListTest = new ArrayList<>();
+        rightRowListTest.add(new DataRow<>(0, "Kyiv"));
+        rightRowListTest.add(new DataRow<>(1, "Berlin"));
+        rightRowListTest.add(new DataRow<>(3, "Budapest"));
+
+        List<JoinedDataRow<Integer, String, String>> joinedRowList = new ArrayList<>();
+        joinedRowList.add(new JoinedDataRow<>(0, "Ukraine", "Kyiv"));
+        joinedRowList.add(new JoinedDataRow<>(1, "Germany", "Berlin"));
+
+        Assertions.assertEquals(joinedRowList, new InnerJoinOperation().join(leftRowListTest, rightRowListTest));
+    }
+
+    @Test
+    void nullObject(){
+        List<DataRow<Integer, String>> leftRowListTest = new ArrayList<>();
+        leftRowListTest.add(null);
+        leftRowListTest.add(null);
+        leftRowListTest.add(new DataRow<>(0, "Ukraine"));
+        leftRowListTest.add(null);
+        leftRowListTest.add(null);
+        leftRowListTest.add(new DataRow<>(1, "Germany"));
+        leftRowListTest.add(null);
+        leftRowListTest.add(new DataRow<>(2, "France"));
+        leftRowListTest.add(null);
+
+        List<DataRow<Integer, String>> rightRowListTest = new ArrayList<>();
+        leftRowListTest.add(null);
+        leftRowListTest.add(null);
+        rightRowListTest.add(new DataRow<>(0, "Kyiv"));
+        leftRowListTest.add(null);
+        leftRowListTest.add(null);
+        rightRowListTest.add(new DataRow<>(1, "Berlin"));
+        leftRowListTest.add(null);
+        rightRowListTest.add(new DataRow<>(3, "Budapest"));
+        leftRowListTest.add(null);
+
+        List<JoinedDataRow<Integer, String, String>> joinedRowList = new ArrayList<>();
+        joinedRowList.add(new JoinedDataRow<>(0, "Ukraine", "Kyiv"));
+        joinedRowList.add(new JoinedDataRow<>(1, "Germany", "Berlin"));
+
+        Assertions.assertEquals(joinedRowList, new InnerJoinOperation().join(leftRowListTest, rightRowListTest));
+    }
+}
---
+++ src/test/java/shaparenko/task/RightJoinOperationTest.java
@@ -0,0 +1,64 @@
+package shaparenko.task;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RightJoinOperationTest {
+
+    @Test
+    void join() {
+        List<DataRow<Integer, String>> leftRowListTest = new ArrayList<>();
+        leftRowListTest.add(new DataRow<>(0, "Ukraine"));
+        leftRowListTest.add(new DataRow<>(1, "Germany"));
+        leftRowListTest.add(new DataRow<>(2, "France"));
+
+        List<DataRow<Integer, String>> rightRowListTest = new ArrayList<>();
+        rightRowListTest.add(new DataRow<>(0, "Kyiv"));
+        rightRowListTest.add(new DataRow<>(1, "Berlin"));
+        rightRowListTest.add(new DataRow<>(3, "Budapest"));
+
+        List<JoinedDataRow<Integer, String, String>> joinedRowList = new ArrayList<>();
+        joinedRowList.add(new JoinedDataRow<>(0, "Ukraine", "Kyiv"));
+        joinedRowList.add(new JoinedDataRow<>(1, "Germany", "Berlin"));
+        joinedRowList.add(new JoinedDataRow<>(3, null, "Budapest"));
+
+        Assertions.assertEquals(joinedRowList, new RightJoinOperation().join(leftRowListTest, rightRowListTest));
+    }
+
+    @Test
+    void nullObject(){
+        List<DataRow<Integer, String>> leftRowListTest = new ArrayList<>();
+        leftRowListTest.add(null);
+        leftRowListTest.add(null);
+        leftRowListTest.add(new DataRow<>(0, "Ukraine"));
+        leftRowListTest.add(null);
+        leftRowListTest.add(null);
+        leftRowListTest.add(new DataRow<>(1, "Germany"));
+        leftRowListTest.add(null);
+        leftRowListTest.add(new DataRow<>(2, "France"));
+        leftRowListTest.add(null);
+
+        List<DataRow<Integer, String>> rightRowListTest = new ArrayList<>();
+        leftRowListTest.add(null);
+        leftRowListTest.add(null);
+        rightRowListTest.add(new DataRow<>(0, "Kyiv"));
+        leftRowListTest.add(null);
+        leftRowListTest.add(null);
+        rightRowListTest.add(new DataRow<>(1, "Berlin"));
+        leftRowListTest.add(null);
+        rightRowListTest.add(new DataRow<>(3, "Budapest"));
+        leftRowListTest.add(null);
+
+        List<JoinedDataRow<Integer, String, String>> joinedRowList = new ArrayList<>();
+        joinedRowList.add(new JoinedDataRow<>(0, "Ukraine", "Kyiv"));
+        joinedRowList.add(new JoinedDataRow<>(1, "Germany", "Berlin"));
+        joinedRowList.add(new JoinedDataRow<>(3, null, "Budapest"));
+
+        Assertions.assertEquals(joinedRowList, new RightJoinOperation().join(leftRowListTest, rightRowListTest));
+    }
+}
