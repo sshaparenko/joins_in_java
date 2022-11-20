9ad7cb072960bed6ed723c5c5aad74d647e46137
sshaparenko <shaparenko.archicgi@gmail.com>
2022-11-20 13:38:52 +0200

    Main class changes

--- src/main/java/shaparenko/task/App.java
+++ src/main/java/shaparenko/task/App.java
@@ -10,26 +10,39 @@ public class App {
         leftRowList.add(new DataRow<>(0, "Ukraine"));
         leftRowList.add(new DataRow<>(1, "Germany"));
         leftRowList.add(new DataRow<>(2, "France"));
-        leftRowList.add(new DataRow<>(5, "arr"));
+
 
         List<DataRow<Integer, String>> rightRowList = new ArrayList<>();
         rightRowList.add(new DataRow<>(0, "Kyiv"));
         rightRowList.add(new DataRow<>(1, "Berlin"));
-        rightRowList.add(new DataRow<>(5, "Budapest"));
-        rightRowList.add(new DataRow<>(2, "2"));
-        rightRowList.add(new DataRow<>(4, "4"));
-        rightRowList.add(new DataRow<>(6, "6"));
+        rightRowList.add(new DataRow<>(3, "Budapest"));
 
-        System.out.println("InnerJoin");
+        System.out.println("Left Row:");
+        System.out.println("--------------");
+        System.out.println(leftRowList);
+        System.out.println();
+
+        System.out.println("Right Row:");
+        System.out.println("--------------");
+        System.out.println(rightRowList);
+        System.out.println();
+
+        System.out.println("InnerJoin:");
+        System.out.println("--------------");
         InnerJoinOperation innerJoin = new InnerJoinOperation();
         System.out.println(innerJoin.join(leftRowList, rightRowList).toString());
+        System.out.println();
 
-        System.out.println("LeftJoin");
+        System.out.println("LeftJoin:");
+        System.out.println("--------------");
         LeftJoinOperation leftJoin = new LeftJoinOperation();
         System.out.println(leftJoin.join(leftRowList, rightRowList));
+        System.out.println();
 
-        System.out.println("RightJoin");
+        System.out.println("RightJoin:");
+        System.out.println("--------------");
         RightJoinOperation rightJoin = new RightJoinOperation();
         System.out.println(rightJoin.join(leftRowList, rightRowList));
+        System.out.println();
     }
 }
